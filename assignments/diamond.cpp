88af26f187e1cd98b33344061b2c3f5b4855ef16
Zeheng Li <imzehengl@gmail.com>
2022-10-05 10:04:01 -0600

    add solutions

--- assignments/diamond.cpp
+++ assignments/diamond.cpp
@@ -4,4 +4,53 @@ using namespace std;
 
 int main()
 {
+    int row;
+    while (true)
+    {
+        cout << "Enter number of rows: ";
+        cin >> row;
+        if (row % 2)
+        {
+            break;
+        }
+    }
+
+    int i, j;
+
+    // upper part
+    for (i = 0; i < row / 2; i++)
+    {
+        for (j = 0; j < row / 2 - i; j++)
+        {
+            cout << " ";
+        }
+        for (j = 0; j < 2 * i + 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // middle
+    for (j = 0; j < row; j++)
+    {
+        cout << "*";
+    }
+    cout << endl;
+
+    // bottom part
+    for (i = row / 2 + 1; i < row; i++)
+    {
+        for (j = 0; j < i - row / 2; j++)
+        {
+            cout << " ";
+        }
+        for (j = 0; j < 2 * (row - i) - 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
--- assignments/print-triangle.cpp
+++ assignments/print-triangle.cpp
@@ -4,4 +4,21 @@ using namespace std;
 
 int main()
 {
+    int row;
+    cout << "Enter number of rows for the triangle: ";
+    cin >> row;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = row - i - 1; j > 0; j--)
+        {
+            cout << " ";
+        }
+        for (int k = 0; k < i + 1; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
